372d246dd5133ac4d8c1145a3951ae8903c6783a
MadhuriManjushaP <53276271+MadhuriManjushaP@users.noreply.github.com>
2019-08-08 00:22:46 +0530

    Add files via upload

---
+++ ASP_basic/CookiesDemo.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CookiesDemo.aspx.cs" Inherits="Cookiesdemo.CookiesDemo" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Cookies Demo</title>
+</head>
+<body>
+    <h2>List Available Cookies</h2>
+    <form id="form1" runat="server">
+        <div>
+            Cookies Name:
+            <asp:TextBox ID="Cname" runat="server"></asp:TextBox><br />
+            Cookie Value:
+            <asp:TextBox ID="Cvalue" runat="server"></asp:TextBox><br />
+            <asp:CheckBox ID="chkDurable" runat="server" Text="Durable Cookie" /><br />
+            <asp:Button ID="btnAdd" runat="server" Text="Add Cookie" OnClick="btnAdd_Click" /><br />
+            <asp:LinkButton ID="LinkButton1" runat="server" OnClick="LinkButton1_Click">List Available Cookies</asp:LinkButton>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP_basic/CookiesDemo.aspx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Cookiesdemo
+{
+    public partial class CookiesDemo : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            HttpCookie c = new HttpCookie(Cname.Text, Cvalue.Text);
+            if (chkDurable.Checked)
+                c.Expires = DateTime.Now.AddHours(1); //adds 1 hour to expiry if checked
+            Response.Cookies.Add(c);
+            Response.Write("Cookie Added");
+        }
+        //lists all cookies created in browser
+        protected void LinkButton1_Click(object sender, EventArgs e)
+        {
+            foreach (String name in Request.Cookies)
+                Response.Write(name + ":" + Request.Cookies[name].Value + "<br/>");
+        }
+    }
+}
